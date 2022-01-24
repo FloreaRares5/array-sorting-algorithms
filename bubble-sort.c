defe4d18ba44c5c9adcafc63062b9e8144152a42
FloreaRares5 <raresflorea05@gmail.com>
2022-01-24 15:49:59 +0200

    Add Bubble Sort

---
+++ bubble-sort.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+static void sort(int a, int b)
+{
+int aux;
+axu = a;
+a = b;
+b = aux;
+}
+
+int main(void)
+{
+int a,b;
+scanf("%d", &a);
+scanf("%d", &b0;
+static void sort(a,b);
+printf("%d", a);
+} 
